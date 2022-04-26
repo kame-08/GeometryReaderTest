96066d25df006946b22f6ddbf4eedd612a6ca892
Kame15-2021 <21cm0108@jec.ac.jp>
2022-04-26 23:32:45 +0900

    First

--- GeometryReaderTest/ContentView.swift
+++ GeometryReaderTest/ContentView.swift
@@ -9,9 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //画面横幅
+        let width = Int(UIScreen.main.bounds.width)
+        //画面高さ
+        let height = Int(UIScreen.main.bounds.height)
+        
+        let idoux=0
+        
+        VStack {
+                    GeometryReader { geometry in
+                        
+                        let xzahyou = Int(geometry.frame(in: .global).origin.x)
+                        
+                                Text("X: \(xzahyou)\nY: \(geometry.frame(in: .global).origin.y)\nwidth: \(geometry.frame(in: .global).width)\nheight: \(geometry.frame(in: .global).height)\nmaxY: \(Int(geometry.frame(in: .local).maxY))\n\(height)")
+                                .font(.largeTitle)
+                        
+                        
+                        
+                       
+                        
+                    }.frame(height: 300)
+                .offset(x: CGFloat(idoux), y: 0)
+            Spacer() }
+            .edgesIgnoringSafeArea(.all)
+        
+     
+        
+        
     }
+     
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
