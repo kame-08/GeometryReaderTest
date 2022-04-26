c9ee12ad88d0a7dd515d6b21c16b18d6caab2eba
Kame15-2021 <21cm0108@jec.ac.jp>
2022-04-26 22:17:03 +0900

    Initial Commit

---
+++ GeometryReaderTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryReaderTest
+//
+//  Created by Ryo on 2022/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeometryReaderTest/GeometryReaderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderTestApp.swift
+//  GeometryReaderTest
+//
+//  Created by Ryo on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
