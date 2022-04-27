d5508a1e1a555c68116b12073cd5ebc8198b0aab
Ryo <21cm0108@jec.ac.jp>
2022-04-27 09:36:25 +0900

    Add:Modelの追加

--- GeometryReaderTest/ContentView.swift
+++ GeometryReaderTest/ContentView.swift
@@ -9,36 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //画面横幅
-        let width = Int(UIScreen.main.bounds.width)
-        //画面高さ
-        let height = Int(UIScreen.main.bounds.height)
         
-        let idoux=0
-        
-        VStack {
-                    GeometryReader { geometry in
-                        
-                        let xzahyou = Int(geometry.frame(in: .global).origin.x)
-                        
-                                Text("X: \(xzahyou)\nY: \(geometry.frame(in: .global).origin.y)\nwidth: \(geometry.frame(in: .global).width)\nheight: \(geometry.frame(in: .global).height)\nmaxY: \(Int(geometry.frame(in: .local).maxY))\n\(height)")
-                                .font(.largeTitle)
-                        
-                        
-                        
-                       
-                        
-                    }.frame(height: 300)
+        GeometryReader { geometry in
+            var idoux=0
+            //画面横幅
+            let width = UIScreen.main.bounds.width
+            //画面高さ
+            let height = UIScreen.main.bounds.height
+            //x座標
+            let xzahyou = geometry.frame(in: .global).origin.x
+            //y座標
+            let yzahyou = geometry.frame(in: .global).origin.y
+            
+            Text("X: \(xzahyou)\nY: \(yzahyou)\nwidth: \(geometry.frame(in: .global).width)\nheight: \(geometry.frame(in: .global).height)\nmaxY: \(Int(geometry.frame(in: .local).maxY))\n\(height)\n\(width)")
+                .font(.largeTitle)
+            
+            //.frame(width:width, height: height)
                 .offset(x: CGFloat(idoux), y: 0)
-            Spacer() }
-            .edgesIgnoringSafeArea(.all)
-        
-     
-        
-        
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        }
+        .edgesIgnoringSafeArea(.all)
     }
-     
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ GeometryReaderTest/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  GeometryReaderTest
+//
+//  Created by Ryo on 2022/04/27.
+//
+
+import Foundation
+
+
